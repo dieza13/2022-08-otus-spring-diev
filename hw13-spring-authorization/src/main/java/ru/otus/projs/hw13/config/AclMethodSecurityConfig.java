daf651a9dcf646cd786877a0b9644897d2bbc7b4
DievAN <DievAN@alfastrah.ru>
2022-12-22 14:25:26 +0300

    hw13-spring-authorization: правки по замечаниям

--- hw13-spring-authorization/src/main/java/ru/otus/projs/hw13/config/AclMethodSecurityConfig.java
+++ hw13-spring-authorization/src/main/java/ru/otus/projs/hw13/config/AclMethodSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.security.access.expression.method.MethodSecurityExpre
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class AclMethodSecurityConfig extends GlobalMethodSecurityConfiguration {
 
